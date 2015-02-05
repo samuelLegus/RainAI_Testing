e89c3bde9cea8771201b0c4b37e628717bf737b0
samuelLegus <samuel.legus@live.com>
2015-02-04 17:36:55 -0800

    Progress! Moving done shooting next

---
+++ Assets/AI/Actions/Shoot.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Action;
+using RAIN.Core;
+
+[RAINAction]
+public class Shoot : RAINAction
+{
+	private WeaponScript _weaponScript;
+	private GameObject _sightedPlayer;
+
+    public override void Start(RAIN.Core.AI ai)
+    {
+        base.Start(ai);
+		_weaponScript = ai.Body.GetComponent<WeaponScript>(); //Retrieve the weapon script from our AI's Unity GameObject
+	}
+
+    public override ActionResult Execute(RAIN.Core.AI ai)
+    {
+		_sightedPlayer = ai.WorkingMemory.GetItem<GameObject>("sightedPlayer");
+		if(_sightedPlayer != null)
+		{
+			_weaponScript._Target = _sightedPlayer;
+			//Debug.DrawRay ( ai.Body.transform.position, 
+			_weaponScript.Fire ();
+	        return ActionResult.SUCCESS;
+		}
+		return ActionResult.FAILURE;
+    }
+
+    public override void Stop(RAIN.Core.AI ai)
+    {
+        base.Stop(ai);
+    }
+}
---
+++ Assets/BulletScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//All motion of the bullet is controlled by rigidbody.
+//Bullets need to be contained in a "Projectiles" physics layer , and the physics later needs to be setup so that the projectiles 
+//do not collide with themselves.
+
+[RequireComponent(typeof(Rigidbody))]
+public class BulletScript : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+	
+	void OnCollisionEnter(Collision col)
+	{
+		Destroy(gameObject);
+	}
+}
